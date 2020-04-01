83797e1ef2eaf1e3478dff9f64a31c8e4ef63aa2
John Bosco Matanda <jbmatanda123@hotmail.com>
2020-04-01 20:49:52 +0200

    Add project files.

---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+    }
+}
